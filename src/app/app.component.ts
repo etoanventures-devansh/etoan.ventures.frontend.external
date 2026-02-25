2991b9c0d18c957503ed1585e21534d0de808e1d
Devansh Chaudhary <devansh@Devanshs-MacBook-Air.local>
2026-02-26 01:16:50 +0800

    Added Components + Images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [FooterComponent]
 })
 export class AppComponent {
   title = 'Etoan-Ventures-Frontend-External';
